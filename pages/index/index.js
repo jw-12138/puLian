c00a696b7d7e3a3e9036cf60fd3de7f775ff2cdf
Jacky <29943110+jw-12138@users.noreply.github.com>
2021-10-28 23:15:48 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+const app = getApp()
+
+Page({
+  data: {
+    linkTree: [
+      {
+        type: "text",
+        name: "h3",
+        text: "用户页面",
+        children: [
+          {
+            name: "ul",
+            children: [
+              {
+                name: "li",
+                
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  onLoad() {
+  
+  }
+})
